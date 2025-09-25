b3ddc8c94ae7eb21433211a2b7a718b0b42a08fd
Alex DeWitt <dwitty7@iastate.edu>
2025-09-24 19:23:45 -0500

    setup button mapping

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MusicActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MusicActivity.java
@@ -1,14 +1,12 @@
 package com.example.androidexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
 
 /*
 
@@ -39,39 +37,35 @@ import org.w3c.dom.Text;
  */
 
 
-public class HomeActivity extends AppCompatActivity {
+public class MusicActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
     private TextView usernameText;  // define username textview variable
     private Button profileButton;    // define profile button variable
-    private Button musicButton;      // define music button variable
+    private Button homeButton;      // define music button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);             // link to Main activity XML
+        setContentView(R.layout.activity_music);             // link to Main activity XML
 
         /* initialize UI elements */
-        messageText = findViewById(R.id.home_msg_txt);      // link to message textview in the Main activity XML
-        usernameText = findViewById(R.id.home_username_txt);// link to username textview in the Main activity XML
-        musicButton = findViewById(R.id.music_button_btn);    // link to music button in the Main activity XML
+        homeButton = findViewById(R.id.home_button_btn);    // link to music button in the Main activity XML)
         profileButton = findViewById(R.id.profile_button_btn);// link to profile button in the Main activity XML
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
-            messageText.setText("Cyclone Sounds");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+                         // set username text invisible initially
         } else {
-            messageText.setText("Welcome");
-            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
+
 
         }
 
-        musicButton.setOnClickListener(new View.OnClickListener() {
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, MusicActivity.class);
+                Intent intent = new Intent(MusicActivity.this, HomeActivity.class);
                 intent.putExtra("USERNAME", extras.getString("USERNAME"));  // key-value to pass to the MainActivity
                 startActivity(intent);
             }
@@ -80,12 +74,10 @@ public class HomeActivity extends AppCompatActivity {
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                Intent intent = new Intent(MusicActivity.this, ProfileActivity.class);
                 intent.putExtra("USERNAME", extras.getString("USERNAME"));  // key-value to pass to the MainActivity
                 startActivity(intent);
             }
         });
-
-
     }
 }
--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -39,48 +39,43 @@ import org.w3c.dom.Text;
  */
 
 
-public class HomeActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
     private TextView usernameText;  // define username textview variable
-    private Button profileButton;    // define profile button variable
     private Button musicButton;      // define music button variable
-
+    private Button homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);             // link to Main activity XML
+        setContentView(R.layout.activity_profile);             // link to Main activity XML
 
         /* initialize UI elements */
-        messageText = findViewById(R.id.home_msg_txt);      // link to message textview in the Main activity XML
-        usernameText = findViewById(R.id.home_username_txt);// link to username textview in the Main activity XML
         musicButton = findViewById(R.id.music_button_btn);    // link to music button in the Main activity XML
-        profileButton = findViewById(R.id.profile_button_btn);// link to profile button in the Main activity XML
+        homeButton = findViewById(R.id.home_button_btn);
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
-            messageText.setText("Cyclone Sounds");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+                         // set username text invisible initially
         } else {
-            messageText.setText("Welcome");
-            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
+
 
         }
 
         musicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, MusicActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, MusicActivity.class);
                 intent.putExtra("USERNAME", extras.getString("USERNAME"));  // key-value to pass to the MainActivity
                 startActivity(intent);
             }
         });
 
-        profileButton.setOnClickListener(new View.OnClickListener() {
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
                 intent.putExtra("USERNAME", extras.getString("USERNAME"));  // key-value to pass to the MainActivity
                 startActivity(intent);
             }
