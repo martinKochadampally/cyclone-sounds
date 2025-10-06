409b7b71e9fce7901ab20e9615b2821c95879675
Kochadampally, Martin S <martink5@iastate.edu>
2025-10-06 11:08:43 -0500

    Edit Main.java

--- Backend/src/main/java/cycloneSounds/Main.java
+++ Backend/src/main/java/cycloneSounds/Main.java
@@ -4,6 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import cycloneSounds.Credentials.Credentials;
+import cycloneSounds.Credentials.CredentialRepository;
+
 import cycloneSounds.profilePage.Profile;
 import cycloneSounds.profilePage.ProfileRepository;
 
@@ -11,6 +16,7 @@ import cycloneSounds.profilePage.ProfileRepository;
  * @author Vivek Bengre
  */
 @SpringBootApplication
+@EnableJpaRepositories
 public class Main {
 
     public static void main(String[] args) {
