b2f5cea58a7c05dd5ff0049a55f48f5bd879b33a
mgsew <mgseward@iastate.edu>
2025-11-03 17:19:09 -0600

    Changes in Friends

--- Backend/src/main/java/cycloneSounds/Friends/FriendsController.java
+++ Backend/src/main/java/cycloneSounds/Friends/FriendsController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("friends")
+@RequestMapping("/api/friends")
 public class FriendsController {
 
     private final FriendsService friendsService;
--- Backend/src/main/java/cycloneSounds/chat/ChatSocket.java
+++ Backend/src/main/java/cycloneSounds/chat/ChatSocket.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@ServerEndpoint(value = "/chat/{username}", configurator = CustomSpringConfigurator.class)
+@ServerEndpoint(value = "/api/chat/{username}", configurator = CustomSpringConfigurator.class)
 public class ChatSocket {
 
     private static final Map<Session, String> sessionUsernameMap = new ConcurrentHashMap<>();
