bb28bb8c50f6e27a7bbfd72113de6406cd5532d0
mgsew <mgseward@iastate.edu>
2025-12-08 22:13:42 -0600

    Adjusted testing again

--- Backend/src/test/java/cycloneSounds/MarkSystemTest.java
+++ Backend/src/test/java/cycloneSounds/MarkSystemTest.java
@@ -15,7 +15,6 @@ import cycloneSounds.Songs.SongRepository;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-//Runner test
 @RunWith(SpringRunner.class)
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Main.class)
@@ -55,15 +54,20 @@ public class MarkSystemTest {
 
     @Test
     public void addSongToPlaylistCheck() {
+        // 1. Create User
         createHelperUser("mgseward", "Mark");
+
         RestAssured.given()
                 .param("playlistName", "Study")
                 .param("username", "mgseward")
                 .post("/api/playlists/create");
 
-        Song testSong = new Song("Runnin", "21 Savage, Metro Boomin");
-        testSong.setSpotifyId("testSpotifyId123");
-        songRepository.save(testSong);
+        String targetSpotifyId = "testSpotifyId123";
+        if (songRepository.findBySpotifyId(targetSpotifyId).isEmpty()) {
+            Song testSong = new Song("Runnin", "21 Savage, Metro Boomin");
+            testSong.setSpotifyId(targetSpotifyId);
+            songRepository.save(testSong);
+        }
 
         Response response = RestAssured.given()
                 .param("songName", "Runnin")
@@ -74,7 +78,6 @@ public class MarkSystemTest {
         assertEquals(200, response.getStatusCode());
         assertTrue(response.getBody().asString().contains("Runnin"));
     }
-
     /**
      * Helper method that makes user creation easy
      */
